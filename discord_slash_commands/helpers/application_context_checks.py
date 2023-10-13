11c0c2f481fed92cfdc13aa281f42a8eb218ff7a
Jasmine Callon <wispwolf@gmail.com>
2023-10-13 13:31:09 -0700

    Fix application_context_checks.py responses

--- discord_slash_commands/helpers/application_context_checks.py
+++ discord_slash_commands/helpers/application_context_checks.py
@@ -30,24 +30,24 @@ def assert_bot_is_in_voice_chat(application_context: discord.ApplicationContext)
     return application_context_check(
         application_context,
         lambda app_ctx: len(app_ctx.bot.voice_clients) != 0 and app_ctx.bot.voice_clients[0].is_connected(),
-        "I must not be connected to a voice chat to use this command." + \
-         "\nPlease disconnect me from voice chat via \\voice leave.")
+        "I must be connected to a voice chat to use this command." + \
+         "\nPlease connect me to a voice channel to speak in via `/voice join`.")
 
 # Asser the bot *is not* in voice chat
 def assert_bot_is_not_in_voice_chat(application_context: discord.ApplicationContext):
     return application_context_check(
         application_context,
-        lambda app_ctx: len(app_ctx.bot.voice_clients) == 0 or app_ctx.bot.voice_clients[0].is_connected(),
-        "I must be connected to a voice chat to use this command." + \
-        "\nPlease connect me to a voice channel to speak in via \\voice join.")
+        lambda app_ctx: not(len(app_ctx.bot.voice_clients) != 0 and app_ctx.bot.voice_clients[0].is_connected()),
+        "I must not be connected to a voice chat to use this command." + \
+        "\nPlease disconnect me from voice chat via `/voice leave`.")
 
 # Assert the bot *is in the same voice channel* as the calling user
 def assert_bot_is_in_same_voice_chat_as_user(application_context: discord.ApplicationContext):
     return application_context_check(
         application_context,
-        lambda app_ctx: assert_bot_is_in_voice_chat(app_ctx) and app_ctx.author in app_ctx.bot.voice_clients[0].channel.members,
+        lambda app_ctx: assert_bot_is_in_voice_chat(app_ctx) and (app_ctx.author in app_ctx.bot.voice_clients[0].channel.members),
         "You must be in the same voice chat as me to use this command." + \
-        "\nPlease connect to the voice chat I am in, or make me join your voice channel via \\voice leave then \\voice join.")
+        "\nPlease connect to the voice chat I am in, or make me join your voice channel via `/voice leave` then `/voice join`.")
 
 # Assert the bot *is* playing audio in voice chat
 def assert_bot_is_playing_audio_in_voice_chat(application_context: discord.ApplicationContext):
@@ -55,7 +55,7 @@ def assert_bot_is_playing_audio_in_voice_chat(application_context: discord.Appli
         application_context,
         lambda app_ctx: assert_bot_is_in_voice_chat(app_ctx) and app_ctx.bot.voice_clients[0].is_playing(),
         "I must already be playing other audio to use this command." + \
-        "\nThis can be done with many commands, such as \\tts play.")
+        "\nThis can be done with many commands, such as `/tts play`.")
 
 # Assert the bot *is not* playing audio in voice chat
 def assert_bot_is_not_playing_audio_in_voice_chat(application_context: discord.ApplicationContext):
@@ -63,4 +63,4 @@ def assert_bot_is_not_playing_audio_in_voice_chat(application_context: discord.A
         application_context,
         lambda app_ctx: not(assert_bot_is_in_voice_chat(app_ctx) and app_ctx.bot.voice_clients[0].is_playing()),
         "I must not already be playing other audio to use this command." + \
-        "\nPlease wait until I finish playing my current audio, or stop my current audio via \\voice stop.")
+        "\nPlease wait until I finish playing my current audio, or stop my current audio via `/voice stop`.")
