25a83e0910c9f0c6fdff4b4b9e1125b189cc5b80
Jasmine Callon <wispwolf@gmail.com>
2023-10-13 14:03:33 -0700

    Clean json_list.py

--- discord_slash_commands/helpers/json_list.py
+++ discord_slash_commands/helpers/json_list.py
@@ -17,15 +17,15 @@ import time
 
 # To be usable by JSONList, a list_type_instance must have (and redefine) the functions defined by this purely virtual type
 class JSONListItem:
-    # Convert dict into member variables
+    # Convert member variables to dictionary
     def to_dict(self) -> dict:
         raise NotImplementedError
 
-    # Convert member variables into dict
+    # Read member variables from dictionary
     def from_dict(self, dictionary: dict) -> None:
         raise NotImplementedError
 
-    # Return a copy of this JSTONListItem (return by value instead of by reference)
+    # Return a copy of this JSTONListItem (by value, not by reference)
     def copy(self):
         raise NotImplementedError
     
@@ -41,39 +41,41 @@ class JSONList:
         list_type_instance: JSONListItem,
         max_file_size_in_bytes: int = 50000
     ):
-        # The directory to of the file to read from and write to
+        # The directory of the JSON file to read from and write to
         self.file_directory = file_directory
-        # The name of the file to read from and write to
+        # The name of the JSON file to read from and write to
         self.file_name = file_name
-        # The concatenation of self.directory and self.file_name
+        # The concatenation of self.file_directory and self.file_name
         self.file_path = f"{self.file_directory}/{self.file_name}"
-        # The timestamp of when the file and memory were last synced
+        # The timestamp of when the JSON file and memory were last synced (self.read or self.write was called)
+        # Measured in seconds since the last epoch, see: https://docs.python.org/3/library/time.html
         self.last_sync = 0
-        # The contents of self.json_file are expected to be a list of dicts
+        # The contents of the JSON file, see if it is out of sync with the file via self.is_desynced
+        # The contents of the JSON file are expected to be a list of dicts, where each dict will then be converted into the same type as self.list_type_instance
         self.list = []
-        # Each dict within the list will define the members of a custom class, and this is how we know the type of the custom class
+        # The type of an individual self.list element
         self.list_type_instance = list_type_instance
-        # Keep a max file size to keep bloat on the bot's computer in-check
+        # The max size the JSON file is allowed to get to, to prevent bloat on the bot's computer
         self.max_file_size_in_bytes = max_file_size_in_bytes
-        # Populate self.list and self.list_type_instance
+        # Actually read the JSON file to populate self.last_sync and self.list
         self.read()
 
-    # Return whether the length of data wishing to be stored is allowable, give a warning if it's close
-    def file_size_is_too_big(self, new_file_size_in_bytes: int) -> bool:
+    # Return whether a length exceeds self.max_file_size_in_bytes, and give the bot's computer a warning if it's close
+    def file_size_is_too_big(self, length_in_bytes: int) -> bool:
         # Check if the file size is at or near threshold
-        if new_file_size_in_bytes * 0.75 > self.max_file_size_in_bytes:
+        if length_in_bytes * 0.75 > self.max_file_size_in_bytes:
             # Give the bot owner a warning with possible solutions
-            print(f"{self.file_path} is full or near full, at {new_file_size_in_bytes} bytes of {self.max_file_size_in_bytes} max bytes.")
-            print(f"Please pause or kill the bot and do one of these 3 options:")
-            print(f" 1. Make a backup and manually remove uneeded info")
-            print(f" 2. Increase the max file size")
-            print(f" 3. Make hyper-optimized versions of to_dict and from_dict methods")
+            print(f"{self.file_path} is full or near full, at {length_in_bytes} bytes of {self.max_file_size_in_bytes} max bytes.")
+            print(f"Please consider these 3 options:")
+            print(f" 1. Pause the bot, make a backup of the JSON file, manually remove uneeded info from it, unpause the bot.")
+            print(f" 2. Kill the bot, increase the max file size for this JSON file, restart the bot.")
+            print(f" 3. Optimize the code, such as the to_dict and from_dict methods, or use a database to store information instead of JSON files")
             if file_size_in_bytes > self.max_file_size_in_bytes:
                 # Refuse to read or write a file bigger than self.max_file_size_in_bytes
                 return True
         return False
 
-    # Read the contents of self.file_name (should be a list of dicts)
+    # Read the contents of self.file_path (expecting it to be a JSON-encoded list of dicts)
     def read(self) -> bool:
         # If the file doesn't exist don't bother reading it
         if not os.path.exists(self.file_path):
@@ -87,6 +89,7 @@ class JSONList:
 
         # Open self.file_path for reading
         file_handle = None
+        read_time = time.mktime(time.localtime())
         try:
             file_handle = open(self.file_path, "r")
         except OSError:
@@ -104,7 +107,7 @@ class JSONList:
             return False
 
         # Log time of read
-        self.last_sync = time.mktime(time.localtime())
+        self.last_sync = read_time
 
         # Parse contents of JSON read as a list of dictionaries
         self.list = []
@@ -115,8 +118,7 @@ class JSONList:
         # Return success
         return True
 
-    # Save contents to self.file_path (should save a list of dicts)
-    # TODO: make sure write directory exists
+    # Save contents to self.file_path (should save a JSON-encoded list of dicts)
     def write(self) -> True:
         # Create a list of dicts to save to self.file_path
         list_of_dict = []
@@ -130,9 +132,13 @@ class JSONList:
             return False
 
         # Write to self.file_path, will overwrite old contents and make new file if one did not exist
-        file_handle = open(self.file_path, "w")
-        file_handle.write(json_dump)
-        file_handle.close()
+        try:
+            file_handle = open(self.file_path, "w")
+            file_handle.write(json_dump)
+            file_handle.close()
+        except OSError:
+            print(f"{self.file_path} could not be written to.")
+            return False
 
         # Log time of write
         self.last_sync = time.mktime(time.localtime())
@@ -140,7 +146,7 @@ class JSONList:
         # Return success
         return True
 
-    # Return the index of the first self.list item that makes search_function return True
+    # Return the index of the first element in self.list search_function for search_match return True
     def get_list_item_index(self, search_function, search_match) -> int:
         for i in range(len(self.list)):
             if search_function(self.list[i], search_match) == True:
@@ -155,10 +161,10 @@ class JSONList:
             return True
 
     # If the contents of memory are older than the contents of self.file_path, overwrite memory with the contents of self.file_path
-    # TODO: In (rare, but less rare the bigger the files get) multi-threaded cases, it is possible
-    #       between a self.sync() and self.write() for the contents of self.file_path to get overwritten to by a different thread.
-    #       That means in small time windows, different threads can overwrite each other's changes to the same file.
-    #       This is probably a common problem, maybe there's a library to help me?
+    # TODO: In rare multi-threaded cases, because different threads cannot share file locks (to my knowledge), it is possible for
+    #       one thread to write *while* another a reading, or multiple threads to write at the same time, or overwrite each other's changes
+    #       because they both thought they were synced, and maybe they were! Lots of little finicky stuff. If only one thread writes it
+    #       shouldn't be a problem. But if not... This is probably a common problem, maybe there's a library to help me? Make a proper database?
     def sync(self) -> bool:
         if self.is_desynced():
             return self.read()
