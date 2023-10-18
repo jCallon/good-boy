4becd5a678739110f9409878d4be9b8fe3e2db24
Jasmine Callon <jascallon99@gmail.com>
2023-10-18 14:57:02 -0700

    resolve more pylint warnings in main.py

--- main.py
+++ main.py
@@ -8,55 +8,73 @@ the discord_slash_commands directory, to the Discord-accessible bot.
 # Import libraries                                                             #
 #==============================================================================#
 
-# Import Discord Python API
-import discord
-
 # Import operating system module
 import os
 
+# Import Discord Python API
+import discord
+
 # Import function for loading environment variables
 from dotenv import load_dotenv
+
+# Import all PyCord.SlashCommand desired to be added to the to the bot
+from discord_slash_commands import rng
+from discord_slash_commands import voice
+from discord_slash_commands import tts
 
 #==============================================================================#
 # Define underlying structure                                                  #
 #==============================================================================#
 
-# Declare PyCord Discord bot, the interface between Discord and the bot code
+# Declare PyCord Discord bot, the interface between Discord and the bot code,
+# and add all PyCord.SlashCommand desired to be added to the to the bot
 discord_bot = discord.Bot()
-
-
-
-# Add all desired, individually written PyCord.SlashCommand, to the bot
-import discord_slash_commands
-from discord_slash_commands import rng
 discord_bot.add_application_command(rng.rng_slash_command_group)
-from discord_slash_commands import voice
 discord_bot.add_application_command(voice.voice_slash_command_group)
-from discord_slash_commands import tts
 discord_bot.add_application_command(tts.tts_slash_command_group)
 
 
 
-# Declare event to let bot owner know when the bot is connected to Discord
 @discord_bot.event
 async def on_ready():
+    """Handles on_ready event for discord_bot.
+
+    Prints a string in-console to let the bot owner know when the bot has
+    connected to Discord.
+    """
+    # Print string in console to let bot owner know bot is connected to Discord
     print(f"{discord_bot.user} is ready and online!")
 
 
 
-# Handle event where the bot ran into a run-time error. Some run-time errors
-# have a custom payload to tell the message author exactly what they did wrong.
 @discord_bot.event
 async def on_application_command_error(
     ctx: discord.ApplicationContext,
     error: discord.DiscordException
 ):
+    """Handles the on_application_command_error event for discord_bot.
+
+    Tell the message author a run-time error was raised. If there was a custom
+    payload attached to the error, to tell the message author exactly why the
+    error occured and how to fix it, tell them that that instead.
+
+    Args:
+        ctx: Context information for the flow the error happened from
+        error: The error raised during execution of the author's message
+    """
+    # Check if error has custom payload to deliver to the message author
     if isinstance(error, discord.CheckFailure) and \
         isinstance(error.payload, str):
+        # Deliver error payload to message author
         await ctx.respond(ephemeral=True, content=error.payload)
-    else:
-        await ctx.respond(ephemeral=True, content="Ran into an unknown error.")
-        raise error
+        return
+
+    # Otherwise, just tell the message author there was a error,
+    # we don't know more than that and might not want to say more than that
+    await ctx.respond(ephemeral=True, content="Ran into an unknown error.")
+
+    # Elevate error so bot-owner sees it in-console
+    raise error
 
 
 
@@ -64,7 +82,9 @@ async def on_application_command_error(
 # Run Discord bot                                                              #
 #==============================================================================#
 
-# Get bot token from environment variables and start bot
+# Load environment variables
 load_dotenv()
-bot_token = str(os.getenv("TOKEN"))
-discord_bot.run(bot_token)
+# Get bot token from environment variables
+BOT_TOKEN = str(os.getenv("TOKEN"))
+# Start bot
+discord_bot.run(BOT_TOKEN)
