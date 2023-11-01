e592485fb17340ec34b2f986ce21fa711a9b8429
Jasmine Callon <jascallon99@gmail.com>
2023-11-01 12:17:54 -0700

    cleanup audio_queue.py

--- discord_slash_commands/helpers/audio_queue.py
+++ discord_slash_commands/helpers/audio_queue.py
@@ -1,6 +1,8 @@
-"""TODO.
+"""Functions for 'queueing' audio to play in voice chat.
 
-TODO.
+This file defines some helpers for playing audio consecutively in voice chat.
+The current implementation in this file is very ugly and basic, and open to
+improvements.
 """
 
 #==============================================================================#
@@ -17,9 +19,12 @@ import discord
 # Define underlying structure                                                  #
 #==============================================================================#
 
-# Create an ad-hoc way to queue audio one-after-another
 # TODO: make real audio queue, this pains me
 
+
+
+# Declare global variables that will be set by set_next_source and used by
+# play_next_source
 global next_voice_client
 global next_audio_source
 global next_after_function
@@ -27,24 +32,25 @@ next_voice_client = None
 next_audio_source = None
 next_after_function = None
 
+
+
 def set_next_source(
     voice_client: discord.VoiceClient,
     audio_source: discord.AudioSource,
     after_function: Callable
 ) -> None:
-    """Set variables used on the next call of play_next_audio_queue_source.
+    """Set variables to use for next call of play_next_source.
 
-    Set the global variables that will be used as arguments, sources, etc. in
-    the next play_next_audio_queue_source call. That function, intended to be
-    used as an after function of PyCord.VoiceClient.play(), is not allowed to
-    have any parameters but error, so globals are the only option I see for now.
+    Set global variables that will be used as arguments, sources, etc. in the
+    next play_next_source call. That function, intended to be used as the
+    'after' argument of PyCord.VoiceClient.play(), is not allowed to have any
+    parameters but error, so globals are the only option I see for now.
 
     Args:
-        voice_client: The voice client play_next_audio_queue_source will play
-            audio on
-        audio_source: The audio source play_next_audio_queue_source will play
-        after_function: The function play_next_audio_queue_source will call
-            when it is done playing audio_source
+        voice_client: The voice client play_next_source will play audio on
+        audio_source: The audio source play_next_source will play
+        after_function: The function play_next_source will call when it is done
+            playing audio_source
     """
     global next_voice_client
     global next_audio_source
@@ -52,6 +58,8 @@ def set_next_source(
     next_voice_client = voice_client
     next_audio_source = audio_source
     next_after_function = after_function
+
+
 
 def play_next_source(error) -> None:
     """Using some globals, make a VoiceClient.play() call.
