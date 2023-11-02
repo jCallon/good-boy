9e83aa93f7a90ea74b88312f689798ccd1723024
Jasmine Callon <wispwolf@gmail.com>
2023-11-02 12:19:13 -0700

    improve comments and whitespace consistency in application_context_checks.py (#9)

--- discord_slash_commands/helpers/application_context_checks.py
+++ discord_slash_commands/helpers/application_context_checks.py
@@ -24,6 +24,14 @@ def application_context_check(
 
     If an application context check passed, return success, otherwise raise a
     CheckFailure with helpful explanations and suggested next steps.
+
+    Args:
+        check_passed: Whether the application context check passed
+        response_if_check_failed: Payload to attach to discord.CheckFailure
+            which will tell the message author what check failed and how to fix.
+
+    Returns:
+        Whether the passed in check passed.
     """
     if check_passed is False:
         failure = discord.CheckFailure
@@ -39,12 +47,20 @@ def assert_bot_is_in_voice_chat(
     """Assert the bot is connected to voice chat.
 
     Assert the bot has an active connection established with a voice channel.
+
+    Args:
+        ctx: The context the slash command using this check was called under
+
+    Returns:
+        Whether the check passed.
     """
     return application_context_check(
         len(ctx.bot.voice_clients) != 0 and \
-        ctx.bot.voice_clients[0].is_connected(),
-        "I must be connected to a voice chat to use this command." + \
-         "\nPlease connect me to a voice chat to speak in via `/voice join`.")
+            ctx.bot.voice_clients[0].is_connected(),
+        "I must be connected to a voice chat to use this command." \
+            + "\nPlease connect me to a voice chat to speak in via " \
+            + "`/voice join`."
+    )
 
 
 
@@ -55,12 +71,19 @@ def assert_bot_is_not_in_voice_chat(
 
     Assert the bot does not have an active connection established with any
     voice channel.
+
+    Args:
+        ctx: The context the slash command using this check was called under
+
+    Returns:
+        Whether the check passed.
     """
     return application_context_check(
         not(len(ctx.bot.voice_clients) != 0 and \
             ctx.bot.voice_clients[0].is_connected()),
-        "I must not be connected to a voice chat to use this command." + \
-        "\nPlease disconnect me from voice chat via `/voice leave`.")
+        "I must not be connected to a voice chat to use this command." \
+            + "\nPlease disconnect me from voice chat via `/voice leave`."
+    )
 
 
 
@@ -71,13 +94,20 @@ def assert_bot_is_in_same_voice_chat_as_author(
 
     Assert the message author is a member within the voice chat it is currently
     connected to.
+
+    Args:
+        ctx: The context the slash command using this check was called under
+
+    Returns:
+        Whether the check passed.
     """
     return application_context_check(
         assert_bot_is_in_voice_chat(ctx) and \
-        (ctx.author in ctx.bot.voice_clients[0].channel.members),
-        "You must be in the same voice chat as me to use this command." + \
-        "\nPlease connect to my voice chat, or make me join your voice " + \
-        "chat via `/voice leave` then `/voice join`.")
+            (ctx.author in ctx.bot.voice_clients[0].channel.members),
+        "You must be in the same voice chat as me to use this command." \
+            + "\nPlease connect to my voice chat, or make me join your " \
+            + "voice chat via `/voice leave` then `/voice join`."
+    )
 
 
 
@@ -89,13 +119,17 @@ def assert_bot_is_playing_audio_in_voice_chat(
     Assert the bot is in voice chat and is currently playing audio in it.
 
     Args:
-        ctx: The context this SlashCommand was called under
+        ctx: The context the slash command using this check was called under
+
+    Returns:
+        Whether the check passed.
     """
     return application_context_check(
         assert_bot_is_in_voice_chat(ctx) and \
-        ctx.bot.voice_clients[0].is_playing(),
-        "I must already be playing other audio to use this command." + \
-        "\nThis can be done via many commands, such as `/tts play`.")
+            ctx.bot.voice_clients[0].is_playing(),
+        "I must already be playing other audio to use this command." \
+            + "\nThis can be done via many commands, such as `/tts play`."
+    )
 
 
 
@@ -107,11 +141,15 @@ def assert_bot_is_not_playing_audio_in_voice_chat(
     Assert the bot is not in voice chat or is not currently playing audio in it.
 
     Args:
-        ctx: The context this SlashCommand was called under
+        ctx: The context the slash command using this check was called under
+
+    Returns:
+        Whether the check passed.
     """
     return application_context_check(
         not(assert_bot_is_in_voice_chat(ctx) and \
             ctx.bot.voice_clients[0].is_playing()),
-        "I must not already be playing other audio to use this command." + \
-        "\nPlease wait until I finish playing my current audio, or stop my " + \
-        "current audio via `/voice stop`.")
+        "I must not already be playing other audio to use this command." \
+            + "\nPlease wait until I finish playing my current audio, or " \
+            + "stop my current audio via `/voice stop`."
+    )
