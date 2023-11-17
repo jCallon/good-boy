c83ab69a88949830bd31989b93d7d6fe523bee6e
Jasmine Callon <jascallon99@gmail.com>
2023-11-17 14:46:16 -0800

    start audio queue work

--- discord_slash_commands/helpers/audio_queue.py
+++ discord_slash_commands/helpers/audio_queue.py
@@ -19,57 +19,359 @@ import discord
 # Define underlying structure                                                  #
 #==============================================================================#
 
-# TODO: make real audio queue, this pains me
+# Define some constants to avoid copy/paste
+MAX_AUDIO_QUEUE_LENGTH = 20
+MIN_VOLUME = .5
+MAX_VOLUME = 2
 
 
 
-# Declare global variables that will be set by set_next_source and used by
-# play_next_source
-global next_voice_client
-global next_audio_source
-global next_after_function
-next_voice_client = None
-next_audio_source = None
-next_after_function = None
+class AudioQueueElement():
+    """Define an instance of information held on audio queued for playing.
 
+    Define what information we should keep track of for each audio source in the
+    audio queue. Most fields are for user-friendliness, so users can identify
+    each audio file in the queue in a way meaningful to them. For example, one
+    of my admins may be interested in seeing who is queuing annoying stuff, and
+    someone wishing to remove a file from the queue will have no idea what the
+    contents of the audio file $hashed_file_name will be.
 
-
-def set_next_source(
-    voice_client: discord.VoiceClient,
-    audio_source: discord.AudioSource,
-    after_function: Callable
-) -> None:
-    """Set variables to use for next call of play_next_source.
-
-    Set global variables that will be used as arguments, sources, etc. in the
-    next play_next_source call. That function, intended to be used as the
-    'after' argument of PyCord.VoiceClient.play(), is not allowed to have any
-    parameters but error, so globals are the only option I see for now.
-
-    Args:
-        voice_client: The voice client play_next_source will play audio on
-        audio_source: The audio source play_next_source will play
-        after_function: The function play_next_source will call when it is done
-            playing audio_source
+    Attributes:
+        audio_queue_element_id: An unique integer identifier for distinguishing
+            this AudioQueueElement from others, independent of position in 
+            AudioQueueList.queue, which may be in constant flux. Used by users
+            trying to modify the audio queue.
+        author_user_id: The ID of the user who added this AudioQueueElement to
+            the AudioQueueList. For users viewing the audio queue.
+            allowed to do certain operations on this AudioQueueItem.
+        source_command: How this AudioQueueElement was added to 
+            AudioQueueList.queue. For users viewing the audio queue.
+        description: A human-readable description of the content of the audio to
+            be played. For users viewing the audio queue. File names are often
+            hashed and not helpful to a user.
+        file_path: The path to the file to actually play once it's this
+            AudioQueueElement's turn to play in voice chat.
     """
-    global next_voice_client
-    global next_audio_source
-    global next_after_function
-    next_voice_client = voice_client
-    next_audio_source = audio_source
-    next_after_function = after_function
+    def __init__(
+        self,
+        audio_queue_element_id: int = 0,
+        author_user_id: int = 0,
+        description: str = "",
+        source_command: str = "",
+        file_name: str = ""
+    ):
+        """Initialize this AudioQueueElement.
 
+        Set the members of this AudioQueueList to their defaults.
 
+        Args:
+            audio_queue_element_id: What to initialize
+                self.audio_queue_element_id as
+            author_user_id: What to initialize self.author_user_id as
+            description: What to initialize self.description as
+            source_command: What to initialize self.source_command as
+            file_name: What to initialize self.file_name as
+        """
+        self.audio_queue_element_id = audio_queue_element_id
+        self.author_user_id = author_user_id
+        self.description = description
+        self.source_command = source_command
+        self.file_path = file_path
 
-def play_next_source(error) -> None:
-    """Using some globals, make a VoiceClient.play() call.
+    def to_str(self) -> None:
+        """Convert this AudioQueueElement to a string.
 
-    Use next_voice_client to play next_audio_source, and after that is done,
-    call next_after_function.
+        Make a string including each member of this AudioQueueElement, formatted
+        in an easy, human-readable way for Discord.
 
-    Args:
-        error: Any errors that happened during the previous VoiceClient.play()
+        Args:
+            self: This AudioQueueElement
+        """
+        return "\nID: `{self.audio_queue_element_id}`" \
+            + "\nAuthor: <@{self.author_user_id}>" \
+            + "\nDescription: `{self.description}`"
+            + "\nSource: `{self.source_command}`" \
+
+    def get_audio_source(
+        self,
+        volume: int = 1.0,
+        offset: int = 0
+    ) -> discord.FFmpegPCMAudio:
+        """Create a Discord-friendly audio source for self.file_path.
+
+        Create an FFMpegPCMAudio audio source for self.file_path, playing from
+        offset seconds after the start, at (volume * 100)% volume.
+
+        Args:
+            self: This AudioQueueElement
+            volume: At what volume to play this audio ((volume * 100)%).
+            offset: How many seconds from the start of an audio to start
+                playing the audio from.
+        """
+        # Check validity of arguments
+        if volume < MIN_VOLUME or volume > MAX_VOLUME or offset < 0:
+            return None
+
+        # Check file still exists
+        with open(self.file_path, "r") as file_handle:
+            # Return audio source
+            try:
+                # vn = disable video
+                # sn = disable subtitles
+                # ac = number of audio channels
+                # ss = at what timestamp to start audio from 
+                # accurate_seek = whether to enable accurate seeking for ss
+                return discord.PCMVolumeTransformer(
+                    original = discord.FFmpegPCMAudio(
+                        source = self.file_path,
+                        options = [
+                            ('vn'),
+                            ('sn'),
+                            ('ac', 1),
+                            ('ss', offset),
+                            ('accurate_seek', 'enable'),
+                        ]
+                    ),
+                    volume = volume
+                )
+            except ClientException:
+                return None
+            
+        # The file does not exist so an audio source coukd not be made
+        print("WARNING: Skipped playing {self.description}, because its file " \
+            + "location, {self.file_path}, could not be openmed and read."
+        return None
+
+class AudioQueueList(commands.Cog):
+    """Define a cog for managing a queue of audio to be played in voice chat.
+
+    Define a Cog with a task to check for outstanding audio to play every 
+    second, while keeping in numerous edge cases, such as pausing, removing
+    currently playing audio, and more.
+
+    Attributes:
+        voice_client: The voice client to play audio on
+        queue: A list of AudioQueueElement to play or store
+        latest_is_finished: Whether queue[0] has finished playing in its
+            entirety and can be deleted.
+        latest_offset: At what time offset into queue[0] to start playing from.
+            Measured in seconds since the start of the audio file.
+        latest_play_timestamp: At what time play() was last called on queue[0].
+            Measured in seconds since the last epoch.
+        paused_audio_was_deleted: Whether the queue[0] paused was deleted,
+            meaing it cannot be resumed on unpause.
+        is_paused: Whether audio queue is currently paused and has stopped
+            playing audio.
     """
-    if error is not None:
-        print(error)
-    next_voice_client.play(next_audio_source, after=next_after_function)
+    def __init__(self, voice_client: discord.VoiceClient):
+        """Initialize this AudioQueueList.
+
+        Set the members of this AudioQueueList to their defaults.
+
+        Args:
+            self: This AudioQueueList
+            voice_client: Waht to initialize self.voice_client as
+        """
+        self.voice_client = voice_client
+        self.queue = []
+        self.latest_is_finished = False
+        self.latest_offset = 0
+        self.latest_play_timestamp = 0
+        self.paused_audio_was_deleted = False
+        self.is_paused = False
+
+    def add(
+        self,
+        ctx: discord.ApplicationContext,
+        description: str,
+        file_path: str
+    ) -> bool:
+        """Add a new AudioQueueElement to this AudioQueueList.
+
+        If there is room in the audio queue, append new AudioQueueElement to the
+        end of self.queue with the passed in parameters and a unique ID.
+
+        Args:
+            self: This AudioQueueList
+            ctx: The ctx of the SlashCommand this function is being called from
+            description: A human-readable description of the audio to play
+            file_path: The path to the audio file to actually play
+
+        Returns:
+            Whether a new AudioQueueElement with your parameters was added.
+        """
+        # Do not allow addition of another audio source if queue is already full
+        if len(self.queue) >= MAX_AUDIO_QUEUE_LENGTH:
+            return False
+
+        # Add a new AudioQueueElement to this AudioQueueList with unique ID.
+        audio_queue_element_list.append(
+            AudioQueueElement(
+                (queue[len(queue) - 1].audio_queue_element_id + 1) % 1000,
+                author_user_id = ctx.author.id,
+                source_command = f"/{ctx.command.qualified_name}",
+                description = description,
+                file_path = file_path
+            )
+        )
+        return True
+
+    def remove(self, audio_queue_element_id: int) -> bool:
+        """Remove an existing AudioQueueElement from this AudioQueueList.
+
+        If an AudioQueueElement matching audio_queue_element_id exists within
+        self.queue, remove it, and stop playing it if it is currently playing.
+
+        Args:
+            self: This AudioQueueList
+            audio_queue_element_id: The ID of the AudioQueueElement to remove
+
+        Returns:
+            Whether the AudioQueueElement asking to be removed could be found
+            and was removed.
+        """
+        # Find the index in queue of the AudioQueueSource with matching ID
+        match_index = -1
+        for i in range(len(self.queue)):
+            if self.queue[i].audio_queue_element_id == audio_queue_element_id:
+                match_index = i
+
+        # If there was no match index, there's nothing to delete, fail
+        if match_index == -1:
+            return False
+
+        # If the match is the audio currently playing, need to stop it
+        if match_index == 1:
+            self.voice_client.stop()
+            if self.is_paused:
+                self.paused_audio_was_deleted = True
+
+        # Remove the matching AudioQueueElement from queue
+        self.queue.pop(match_index)
+        return True
+
+    def pause(self) -> None:
+        """Stop playing audio until unpaused.
+
+        Pause the audio queue, stopping its current audio and saving its
+        progress, so it can be resumed from the same point.
+
+        Args:
+            self: This AudioSourceList
+        """
+        # If already paused don't do anything
+        if self.is_paused is True:
+            return
+
+        # Set is_paused to true so audio does not resume
+        self.is_paused = True
+
+        # Stop currently playing audio and remember its progress
+        self.voice_client.stop()
+        self.latest_offset += time.time() - self.latest_play_timestamp
+
+    def unpause(self) -> None:
+        """Keep playing audio until paused.
+
+        Unpause the audio queue, restoring its progress, if possible, from when
+        it was paused. This may not be possible, if, for example, the audio file
+        has been deleted from queue or disk while the bot was paused.
+
+        Args:
+            self: This AudioSourceList
+        """
+        # If already unpaused don't do anything
+        if self.is_paused is False:
+            return
+
+        # If the audio source being played when the queue was paused was
+        # deleted, just resume the normal audio queue to pick up the next audio
+        if self.paused_audio_was_deleted or len(self.queue) == 0:
+            self.is_paused = False
+            return
+
+        # Create audio source at previous progress
+        # If the file no longer exists or cannot be read, it cannot be resumed
+        audio_source = self.queue[0].get_audio_source(
+            self.volume,
+            self.latest_offset
+        )
+        if audio_source is None:
+            self.is_paused = False
+            return
+
+        # Play audio source, save time of starting play(),
+        # afterwards set as unpaused so normal audio queue can resume
+        self.latest_play_timestamp = time.time()
+        self.voice_client.play(audio_source, after=self.is_paused=False)
+    
+    #def interrupt(self, audio_source: discord.audio_source) -> None:
+    #    """Interrupt the current audio queue to play a more important sound.
+    #    
+    #    TODO.
+    #    """
+    #    # TODO: scenario with multiple interrupts? use differnt audio queue
+    #    # for higher priority sounds?
+    #    self.pause()
+    #    self.voice_client.play(audio_source, after=self.unpause)
+
+    #def volume(self, volume: float) -> bool:
+    #    """TODO.
+    #    
+    #    TODO.
+    #
+    #    Attributes:
+    #        TODO
+    #
+    #    Return:
+    #        TODO.
+    #    """
+    #    # Change volume going forward, deny if unreasonable
+    #    if volume < .5 or volume > 2:
+    #        return False
+    #    self.volume = volume
+    #
+    #    # If audio is currently playing change its volume
+    #    # TODO
+    #
+    #    return True
+
+    # NOTE: There's probably a more efficient way to do this, such as with
+    # events and listeners
+    @tasks.loop(seconds=1.0)
+    def play_next(self) -> None:
+        """Play the next AudioQueueElement in queue.
+        
+        Play queue[0] in voice chat unless paused, already playing something,
+        or there is nothing to play.
+
+        Args:
+            self: This AudioQueueList
+        """
+        # Don't do anything if:
+        # - We are paused
+        # - We are already playing audio
+        # - There are no audio elements queued
+        if self.is_paused or \
+            self.voice_client.is_playing() or \
+            len(self.queue) == 0:
+            return
+
+        # Remove finished audio from queue, reset intermediate state
+        if self.latest_finished is True:
+            self.queue.pop(0)
+            self.latest_offset = 0
+            self.latest_finished = False
+
+        # Create audio_source
+        # If the file no longer exists or cannot be read, it cannot be resumed
+        audio_source = self.queue[0].get_audio_source(
+            self.volume,
+            self.latest_offset
+        )
+        if audio_source is None:
+            return
+
+        # Play audio_source
+        self.latest_play_timestamp = time.time()
+        self.voice_client.play(audio_source, after=self.latest_finished=True)
--- discord_slash_commands/voice.py
+++ discord_slash_commands/voice.py
@@ -64,8 +64,8 @@ async def voice_join(ctx):
         return False
 
     # Join the author's voice chat
-    # TODO: Play a high bark on entry
     await ctx.author.voice.channel.connect()
+    ctx.bot.add_cog(AudioQueue(ctx.bot.voice_clients[0]))
     await ctx.respond(
         ephemeral = False,
         delete_after = 60*30,
@@ -93,8 +93,8 @@ async def voice_leave(ctx):
         ctx: The context this SlashCommand was called under
     """
     # Leave the author's voice chat
-    # TODO: Play a low bark on exit
     await ctx.voice_client.disconnect()
+    ctx.bot.remove_cog("AudioQueue")
     await ctx.respond(
         ephemeral=False,
         delete_after=60*30,
@@ -104,30 +104,155 @@ async def voice_leave(ctx):
 
 
 
-# TODO: Support audio queue,
-#       instead of only being able to play one audio at a time
-@voice_slash_command_group.command(
-    name="stop",
-    description="Make me stop whatever audio I am playing in your voice chat.",
-    checks=[
+#@voice_slash_command_group.command(
+#    name="volume",
+#    description="TODO.",
+#    checks=[]
+#)
+#async def voice_volume(ctx):
+#    """TODO.
+#
+#    TODO.
+#
+#    Args:
+#        ctx: The context this SlashCommand was called under
+#        TODO
+#    """
+#    # TODO
+#    await ctx.respond(ephemeral=True, content="")
+#    return True
+
+
+
+voice_queue_slash_command_group = discord.SlashCommandGroup(
+    checks = [
         ctx_check.assert_bot_is_in_voice_chat,
         ctx_check.assert_bot_is_in_same_voice_chat_as_author,
-        ctx_check.assert_bot_is_playing_audio_in_voice_chat,
-    ]
+        ctx_check.assert_bot_audio_queue_length_is_non_zero,
+    ],
+    #default_member_permissions = default,
+    description = "Voice/audio queue commands",
+    #description_localizations = default,
+    #guild_ids = default,
+    guild_only = True,
+    name = "queue",
+    #name_localizations = default,
+    #nsfw = default,
+    parent = voice_slash_command_group
 )
-async def voice_stop(ctx):
-    """Tell bot to stop currently playing audio.
 
-    Make bot stop playing the audio it is currently playing in your voice chat.
+
+
+@voice_queue_slash_command_group.command(
+    name="remove",
+    description="Skip playing a certain item in the audio queue.",
+)
+async def voice_queue_remove(
+    ctx,
+    audio_queue_element_id: discord.Option(
+        int,
+        description="ID of audio queue item to skip. " \
+            "Leave blank to skip top of queue.",
+        default=None
+    )
+):
+    """Tell bot to remove a certain item from its audio queue.
+
+    Make bot remove the audio queue element specified by audio_queue_element_id
+    from its audio queue. If None is provided, remove the top of the audio
+    queue, effectively skipping whatever is currently playing or will play next.
+
+    Args:
+        ctx: The context this SlashCommand was called under
+        audio_queue_element_id: The audio_queue_element_id of the
+            AudioQueueElement to remove from audio_queue
+    """
+    # Get the audio queue,
+    # fill in the audio_queue_element_id if None was provided
+    audio_queue = bot.ctx.cogs.get_cog("AudioQueue")
+    if audio_queue_element_id is None:
+        audio_queue_element_id = audio_queue.queue[0].audio_queue_element_id
+
+    # Try to remove audio_queue_element_id from audio_queue
+    if audio_queue.remove(audio_queue_element_id) is False:
+        await ctx.respond(
+            ephemeral=True,
+            content="I could not find an item in the audio queue with an ID " \
+                + f"of {audio_queue_element_id}."
+        )
+        return False
+        
+    await ctx.respond(
+        ephemeral=False,
+        delete_after=60,
+        content="I removed the audio queue item with an ID of " \
+            + f"{audio_queue_element_id} from my audio queue."
+    )
+    return True
+
+
+
+@voice_queue_slash_command_group.command(
+    name="pause",
+    description="Pause or unpause my audio queue.",
+)
+async def voice_queue_pause(
+    ctx,
+    value: discord.Option(
+        str,
+        description="Whether to start or stop pausing."
+        choices=["start", "stop"]
+    )
+):
+    """Pause or unpause playing audio in voice chat.
+
+    If starting pause, stop playing audio in voice chat, with the intent to
+    resume the same audio queue, possibly modified, later, from the same spot,
+    if possible. If stopping pause, resume playing audio from the point it was
+    paused, if available, otherwise just what's at the top of audio queue.
+
+    Args:
+        ctx: The context this SlashCommand was called under
+        value: Whether to start or stop pausing audio queue
+    """
+    # Get the audio queue, set whether to pause or unpause it
+    audio_queue = bot.ctx.cogs.get_cog("AudioQueue")
+    if value == "start":
+        audio_queue.pause()
+        await ctx.respond(ephemeral=True, content="I paused my audio queue.")
+    elif value == "stop":
+        audio_queue.unpause()
+        await ctx.respond(ephemeral=True, content="I unpaused my audio queue.")
+
+    return True
+
+
+
+@voice_queue_slash_command_group.command(
+    name="list",
+    description="List what's currently in my audio queue.",
+)
+async def voice_queue_list(ctx):
+    """Tell bot to list audio queue.
+
+    Tell the bot to give you a list and details of all its audio queue elements.
 
     Args:
         ctx: The context this SlashCommand was called under
     """
-    # Stop the playing of the audio currently playing in voice chat
-    ctx.voice_client.stop()
-    await ctx.respond(
-        ephemeral=False,
-        delete_after=60,
-        content="I have tried to stop what was playing in your voice chat."
-    )
+    # Get the audio queue
+    audio_queue = bot.ctx.cogs.get_cog("AudioQueue")
+
+    # Make a list of strings, each list element afer the 1st representing an
+    # AudioQueueElement
+    page_list = ["Summary:" \
+        + "\n`Audio Queue Element ID: First 50 characters of description`"]
+    for audio_queue_element in audio_queue.queue:
+        page_list[0] += f"\n`{audio_queue_element.audio_queue_element_id}: " \
+            + f"{audio_queue_element.description[:49]}`"
+        page_list.append(audio_queue_element.to_str())
+
+    # Return a neat page view of the audio queue
+    paginator = pages.Paginator(pages=page_list, loop_pages=False)
+    await paginator.respond(ctx.interaction, ephemeral=True)
     return True
